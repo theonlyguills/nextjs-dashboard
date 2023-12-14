69e4c40e156dbca07bd2af32ccf33caaeaeb9705
Guillaume Larose <theonlyguills@gmail.com>
2023-12-14 16:39:12 -0500

    oops

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,7 +3,7 @@
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
-export default function Search() {
+export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
@@ -30,6 +30,7 @@ export default function Search() {
           handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
+        placeholder={placeholder}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
