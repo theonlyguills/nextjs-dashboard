7f52ff93624c14fda8e9d5eb7536c509dac6917a
Guillaume Larose <theonlyguills@gmail.com>
2023-12-15 17:46:47 +0000

    update invoice

--- src/app/ui/invoices/buttons.tsx
+++ src/app/ui/invoices/buttons.tsx
@@ -16,9 +16,7 @@ export function CreateInvoice() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
-      className="rounded-md border p-2 hover:bg-gray-100"
-    >
+    href={`/dashboard/invoices/${id}/edit`} className="rounded-md border p-2 hover:bg-gray-100">
       <PencilIcon className="w-5" />
     </Link>
   );
